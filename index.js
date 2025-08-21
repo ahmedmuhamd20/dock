74aa150e9b1cb703e2d53c2a1b221003e5ce2556
ahmedmuhamd20 <81447966+ahmedmuhamd20@users.noreply.github.com>
2025-08-21 00:18:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8000
 
 app.get('/', (req, res) => 
 {
-    return res.json({ message : "runninghi project"})
+    return res.json({ message : "running project"})
 })
 
 app.get('/home', (req, res) => 
